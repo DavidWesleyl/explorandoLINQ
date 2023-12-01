354a88fb8b79cb24253302dd0e4b80409ae75139
David Wesley <109743202+DavidWesleyl@users.noreply.github.com>
2023-12-01 10:51:34 -0300

    Add files via upload

---
+++ ExercicioLinq/ExercicioLinq/Entities/dadosFuncionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioLinq.Entities
+{
+	internal class dadosFuncionario
+	{
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public decimal Salario { get; set; }
+
+		public dadosFuncionario(string nome, string email, decimal salario)
+		{
+			Nome = nome;
+			Email = email;
+			Salario = salario;
+		}
+
+
+		public override string ToString()
+		{
+			return $"Nome: {Nome} | Email: {Email} | Salário: {Salario}";
+		}
+	}
+}
---
+++ ExercicioLinq/ExercicioLinq/Program.cs
@@ -0,0 +1,76 @@
+﻿using ExercicioLinq.Entities;
+using System.Globalization;
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		
+
+		Console.Write("Digite o endereço: ");
+
+		string endereco = Console.ReadLine();
+
+		List<dadosFuncionario> listaDados = new List<dadosFuncionario>();
+
+
+		using(StreamReader leitorEndereco = new StreamReader(endereco)) // Stream Reader => vai ler o arquivo .csv e extrair informações!
+		{
+			while(!leitorEndereco.EndOfStream) 
+			{
+				string[] camposEndereco = leitorEndereco.ReadLine().Split(',');
+
+				string nome = camposEndereco[0];
+				string email = camposEndereco[1];
+				decimal salarioFuncionario = decimal.Parse(camposEndereco[2]);
+
+				listaDados.Add(new dadosFuncionario(nome, email, salarioFuncionario));
+				
+			}
+		}
+
+
+		Console.WriteLine();
+
+        Console.Write("Digite o salario: ");
+
+		decimal salario = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Email das pessoas que o salário são maiores que {salario}");
+
+		// Mostrar email das pessoas cujo o salario é abaixo do informado.
+
+		var informeSalario = listaDados.Where(x => x.Salario > salario).OrderBy(x => x.Nome);
+
+        Console.WriteLine();
+
+
+        foreach (dadosFuncionario informacoes in informeSalario)
+		{
+			Console.WriteLine($"{informacoes.Email}");
+		}
+
+		Console.WriteLine();
+
+		// Soma do salario das pessoa que começam com M 
+
+		Console.WriteLine("Soma do salario das pessoas que começam com M:");
+
+        
+
+        var somaSalario = listaDados.Where(nomes => nomes.Nome.StartsWith('M')).Sum(salarioFinal => salarioFinal.Salario);
+
+        Console.WriteLine();
+
+        Console.WriteLine(somaSalario);
+
+
+
+		
+
+
+
+
+    }
+}
